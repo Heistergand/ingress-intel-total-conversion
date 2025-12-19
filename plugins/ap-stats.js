576b58b960a88a842c3fe4bd3f6c0d42261e17f4
Heistergand <info@heistergand.com>
2025-12-19 10:15:51 +0100

    Heistergand patch ap statistics plugin (#2)
    * Update ap-stats.js
    
    take machina into account, fix issue #879
    
    * Update ap-stats.js
    
    fix eslint
    
    * Update ap-stats.js
    
    convert coffe into code
    
    * Update ap-stats.js

--- plugins/ap-stats.js
+++ plugins/ap-stats.js
@@ -116,7 +116,6 @@ window.plugin.compAPStats.compAPStats = function () {
 
     // AP to destroy this portal
     var destroyAp = (data.resCount || 0) * window.DESTROY_RESONATOR;
-
     if (portal.options.team === window.TEAM_ENL) {
       result.res.AP += destroyAp + PORTAL_FULL_DEPLOY_AP;
       result.res.destroyPortals++;
@@ -131,16 +130,19 @@ window.plugin.compAPStats.compAPStats = function () {
         result.res.AP += completePortalAp;
         result.res.finishPortals++;
       }
+    } else if (portal.options.team === window.TEAM_MAC) {
+      // it's a machina portal, destroy AP for both teams.
+      result.enl.AP += destroyAp + PORTAL_FULL_DEPLOY_AP;
+      result.res.AP += destroyAp + PORTAL_FULL_DEPLOY_AP;
+
+      result.enl.destroyPortals++;
+      result.res.destroyPortals++;
     } else {
-      if (portal.options.team === window.TEAM_MAC) {
-        // it's a machina portal, destroy AP for both teams.
-        result.enl.AP += destroyAp;
-        result.res.AP += destroyAp;
-      }
       // it's a neutral or machina portal, potential for both teams.
       result.enl.AP += PORTAL_FULL_DEPLOY_AP;
-      result.enl.capturePortals++;
       result.res.AP += PORTAL_FULL_DEPLOY_AP;
+
+      result.enl.capturePortals++;
       result.res.capturePortals++;
     }
   });
